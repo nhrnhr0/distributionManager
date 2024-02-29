b5f0a86723752596d288f86fd7c810152b2f1199
nhrnhr0 <nhrnhr0@gmail.com>
2024-02-29 19:26:55 +0200

    first commit, working on models

---
+++ backend/models/admin.py
@@ -0,0 +1,45 @@
+from django.contrib import admin
+from .models import SysUser, Business, Category, ContentSchedule
+from django.utils.safestring import mark_safe
+
+# Register your models here.
+class SysUserAdmin(admin.ModelAdmin):
+    list_display = ['name', 'user']
+    pass
+admin.site.register(SysUser, SysUserAdmin)
+
+
+class BusinessAdmin(admin.ModelAdmin):
+    list_display = ['name', 'slug', 'all_users']
+    prepopulated_fields = {'slug': ('name',)}
+    filter_horizontal = ['users']
+    readonly_fields = ['all_users']
+
+    def all_users(self, obj):
+        return ', '.join([user.name for user in obj.users.all()])
+
+    pass
+admin.site.register(Business, BusinessAdmin)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['id', 'image_display', 'name', 'slug', 'business']
+    prepopulated_fields = {'slug': ('name',)}
+    readonly_fields = ['image_display','all_whatsapp_urls', 'all_telegram_urls', ]
+    
+    search_fields = ['name', 'business__name']
+    list_filter = ['business',]
+    
+    def image_display(self, obj):
+        if obj.icon:
+            return mark_safe(f'<img src="{obj.icon.url}" width="50" height="50" />')
+        return 'No Image'
+    image_display.short_description = 'Icon'
+
+    pass
+admin.site.register(Category, CategoryAdmin)
+class ContentScheduleAdmin(admin.ModelAdmin):
+    list_display = ['id', 'message', 'image', 'approve_telegram_id', 'created_at', 'updated_at', 'business', 'approve_state', 'approve_date', 'reject_reason']
+    filter_horizontal = ['categories']
+    readonly_fields = ['created_at', 'updated_at', 'approve_date', 'reject_reason', 'approve_telegram_id', 'approve_state',]
+    pass
+admin.site.register(ContentSchedule, ContentScheduleAdmin)
---
+++ backend/models/models.py
@@ -0,0 +1,64 @@
+from django.db import models
+
+# Create your models here.
+class SysUser(models.Model):
+    name = models.CharField(max_length=100)
+    user = models.OneToOneField('auth.User', on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return self.name
+
+class Business(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True, allow_unicode=True)
+    
+    users = models.ManyToManyField(SysUser, related_name='businesses')
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Category(models.Model):
+    icon = models.ImageField(upload_to='categories/', blank=True, null=True)
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100,allow_unicode=True)
+    business = models.ForeignKey(Business, on_delete=models.CASCADE, related_name='categories')
+    
+    open_whatsapp_url = models.CharField(max_length=200, blank=True, null=True)
+    open_telegram_url = models.CharField(max_length=200, blank=True, null=True)
+    
+    
+    
+    all_whatsapp_urls = models.JSONField(blank=True, null=True)
+    all_telegram_urls = models.JSONField(blank=True, null=True)
+    
+    def __str__(self) -> str:
+        return self.name + ' - ' + self.business.name
+    
+    class Meta:
+        unique_together = ('name', 'business')
+
+
+# content state options: Approved, Pending, Rejected
+CONTENT_STATE = (
+    ('A', 'Approved'),
+    ('P', 'Pending'),
+    ('R', 'Rejected'),
+)
+
+class ContentSchedule(models.Model):
+    message = models.TextField(max_length=200, blank=True, null=True)
+    image = models.ImageField(upload_to='contents/', blank=True, null=True)
+    send_date = models.DateTimeField(blank=True, null=True)
+    approve_telegram_id = models.CharField(max_length=100, blank=True, null=True)
+    
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    
+    business = models.ForeignKey(Business, on_delete=models.CASCADE, related_name='contentSchedules')
+
+    approve_state = models.CharField(max_length=1, choices=CONTENT_STATE, default='P')
+    approve_date = models.DateTimeField(blank=True, null=True)
+    reject_reason = models.TextField(max_length=200, blank=True, null=True)
+    categories = models.ManyToManyField(Category, related_name='contentSchedules', blank=True)
+    
